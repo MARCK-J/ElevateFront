27a6fcbf61c1f5821385583f6e27bdaeb1190f6a
Camilo <144707673+CamiloMendez23@users.noreply.github.com>
2024-09-17 16:51:07 -0400

    avance en el menu principal del sistema

--- src/views/Presentation/Sections/Data/designBlocksData.js
+++ src/views/Presentation/Sections/Data/designBlocksData.js
@@ -31,9 +31,9 @@ import imgDropdowns from "@/assets/img/dropdowns.jpg";
 
 export default [
   {
-    heading: "Design Blocks",
+    heading: "PROGRAMACION",
     description:
-      "A selection of 45 page sections that fit perfectly in any combination",
+      "Aprende una gran cantidad de lenguajes de programacion",
     items: [
       {
         image: `${imagesPrefix}/headers.jpg`,
@@ -56,88 +56,12 @@ export default [
         route: "presentation",
         pro: true
       },
-      {
-        image: `${imagesPrefix}/faq.jpg`,
-        title: "FAQ",
-        subtitle: "1 Example",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: imgBlogPosts,
-        title: "Blog Posts",
-        subtitle: "11 Examples",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: imgTestimonials,
-        title: "Testimonials",
-        subtitle: "11 Examples",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: imgTeam,
-        title: "Teams",
-        subtitle: "6 Examples",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: imgStat,
-        title: "Stats",
-        subtitle: "3 Examples",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: `${imagesPrefix}/call-to-action.jpg`,
-        title: "Call to Actions",
-        subtitle: "8 Examples",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: `${imagesPrefix}/projects.jpg`,
-        title: "Applications",
-        subtitle: "6 Examples",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: `${imagesPrefix}/logo-area.jpg`,
-        title: "Logo Areas",
-        subtitle: "4 Examples",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: `${imagesPrefix}/footers.jpg`,
-        title: "Footers",
-        subtitle: "10 Examples",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: `${imagesPrefix}/general-cards.jpg`,
-        title: "General Cards",
-        subtitle: "9 Examples",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: imgContent,
-        title: "Content Sections",
-        subtitle: "8 Examples",
-        route: "presentation",
-        pro: true
-      }
+      
     ]
   },
   {
-    heading: "Navigation",
-    description: "30+ components that will help go through the pages",
+    heading: "MATEMATICAS",
+    description: "Desde conceptos basicos hasta los mas avanzados solo aqui",
     items: [
       {
         image: `${imagesPrefix}/navbars.jpg`,
@@ -163,9 +87,9 @@ export default [
     ]
   },
   {
-    heading: "Input Areas",
+    heading: "FISICA",
     description:
-      "50+ elements that you need for text manipulation and insertion",
+      "Aprende como se comportan los cuerpos en el espacio y tiempo en el que nos encontramos",
     items: [
       {
         image: `${imagesPrefix}/newsletters.jpg`,
@@ -188,19 +112,13 @@ export default [
         route: "inputareas-forms",
         pro: false
       },
-      {
-        image: `${imagesPrefix}/inputs.jpg`,
-        title: "Inputs",
-        subtitle: "6 Examples",
-        route: "inputareas-inputs",
-        pro: false
-      }
+      
     ]
   },
   {
-    heading: "Attention Catchers",
+    heading: "QUIMICA",
     description:
-      "20+ Fully coded components that popup from different places of the screen",
+      "Comprende sobre la materia y sus propiedades en estos cursos de quimica",
     items: [
       {
         image: imgAlert,
@@ -223,19 +141,13 @@ export default [
         route: "ac-tooltips-popovers",
         pro: false
       },
-      {
-        image: imgModal,
-        title: "Modals",
-        subtitle: "5 Examples",
-        route: "ac-modals",
-        pro: false
-      }
+      
     ]
   },
   {
-    heading: "Elements",
+    heading: "HISTORIA",
     description:
-      "80+ carefully crafted small elements that come with multiple colors and shapes",
+      "Comprende la historia de la humanidad y como hemos llegado hasta donde estamos",
     items: [
       {
         image: `${imagesPrefix}/buttons.jpg`,
@@ -258,55 +170,7 @@ export default [
         route: "el-dropdowns",
         pro: false
       },
-      {
-        image: `${imagesPrefix}/switch.jpg`,
-        title: "Toggles",
-        subtitle: "2 Examples",
-        route: "el-toggles",
-        pro: false
-      },
-      {
-        image: `${imagesPrefix}/social-buttons.jpg`,
-        title: "Social Buttons",
-        subtitle: "2 Examples",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: `${imagesPrefix}/breadcrumbs.jpg`,
-        title: "Breadcrumbs",
-        subtitle: "1 Example",
-        route: "el-breadcrumbs",
-        pro: false
-      },
-      {
-        image: `${imagesPrefix}/badges.jpg`,
-        title: "Badges",
-        subtitle: "3 Examples",
-        route: "el-badges",
-        pro: false
-      },
-      {
-        image: `${imagesPrefix}/progress.jpg`,
-        title: "Progress Bars",
-        subtitle: "4 Examples",
-        route: "el-progress-bars",
-        pro: false
-      },
-      {
-        image: `${imagesPrefix}/tables.jpg`,
-        title: "Tables",
-        subtitle: "3 Examples",
-        route: "presentation",
-        pro: true
-      },
-      {
-        image: `${imagesPrefix}/typography.jpg`,
-        title: "Typography",
-        subtitle: "2 Examples",
-        route: "el-typography",
-        pro: false
-      }
+      
     ]
   }
 ];
