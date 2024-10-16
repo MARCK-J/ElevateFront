f6faab4c2014c44b1b2465528085f2a2bb58b84e
Luis Huanca <luanhuancam@gmail.com>
2024-10-15 21:36:25 -0400

    Campo para la busqueda de cursos

--- src/router/index.js
+++ src/router/index.js
@@ -69,6 +69,12 @@ const router = createRouter({
       name: "dashboard",
       component: DashboardView,
     },
+    {
+      path: "/courses",
+      name: "coursesList",
+      component: CourseListView,
+    },
+
   ],
 });
 
---
+++ src/views/CourseListView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import BaseLayout from "../layouts/sections/components/BaseLayout.vue";
+import MaterialInput from "@/components/MaterialInput.vue";
+</script>
+
+<template>
+  <BaseLayout
+    title="Listado de Cursos"
+    :breadcrumb="[
+      { label: 'Cursos', route: '/' },
+      { label: 'Busqueda de cursos' },
+    ]"
+  >
+        <div class="row justify-space-between py-0">
+            <MaterialInput
+              class="input-group-dynamic mb-4"
+              icon="search"
+              type="text"
+              placeholder="Busqueda por Titulo, categoria, puntuacion"
+            />
+        </div>
+   
+  </BaseLayout>
+</template>
