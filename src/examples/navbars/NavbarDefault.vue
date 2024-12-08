03ca4ddb44264e187e817dd6f8097552856f3da3
Camilo <144707673+CamiloMendez23@users.noreply.github.com>
2024-12-08 16:41:12 -0400

    botones de cursos y paginas responsive

--- src/examples/navbars/NavbarDefault.vue
+++ src/examples/navbars/NavbarDefault.vue
@@ -4,7 +4,7 @@ import { computed, ref, watch } from "vue";
 import { useWindowsWidth } from "../../assets/js/useWindowsWidth";
 import { useAppStore } from "@/stores"; // Pinia store
 import Swal from "sweetalert2";
-
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import ArrDark from "@/assets/img/down-arrow-dark.svg";
 import DownArrWhite from "@/assets/img/down-arrow-white.svg";
@@ -59,7 +59,7 @@ console.log("Is Logged In:", isLoggedIn.value);
 console.log("Identificador del usuario:", identidad.value);
 console.log("Rol del usuario:", rol.value);
 
-// set arrow  color
+// set arrow color
 function getArrowColor() {
   if (props.transparent && textDark.value) {
     return ArrDark;
@@ -69,12 +69,12 @@ function getArrowColor() {
     return ArrDark;
   }
 }
+
 function cerrarSesion() {
   Swal.fire('Éxito', 'Cierre de sesión exitoso', 'success').then(() => {
     window.location.href = '/'; // Asegúrate de ajustar la ruta a la correcta
   });
 }
-
 
 // set text color
 const getTextColor = () => {
@@ -118,54 +118,40 @@ const showProfileButton = computed(() => isLoggedIn.value);
   <nav
     class="navbar navbar-expand-lg top-0"
     :class="{
-      'z-index-3 w-100 shadow-none navbar-transparent position-absolute my-3':
-        props.transparent,
-      'my-3 blur border-radius-lg z-index-3 py-2 shadow py-2 start-0 end-0 mx-4 position-absolute mt-4':
-        props.sticky,
+      'z-index-3 w-100 shadow-none navbar-transparent position-absolute my-3': props.transparent,
+      'my-3 blur border-radius-lg z-index-3 py-2 shadow py-2 start-0 end-0 mx-4 position-absolute mt-4': props.sticky,
       'navbar-light bg-white py-3': props.light,
       ' navbar-dark bg-gradient-dark z-index-3 py-3': props.dark,
     }"
   >
-    <div
-      :class="
-        props.transparent || props.light || props.dark
-          ? 'container'
-          : 'container-fluid px-0'
-      "
-    >
+    <div :class="props.transparent || props.light || props.dark ? 'container' : 'container-fluid px-0'">
       <RouterLink
-        class="navbar-brand d-none d-md-block"
-        :class="[
-          (props.transparent && textDark.value) || !props.transparent
-            ? 'text-dark font-weight-bolder ms-sm-3'
-            : 'text-white font-weight-bolder ms-sm-3',
-        ]"
+        class="navbar-brand"
+        :class="[(props.transparent && textDark.value) || !props.transparent ? 'text-dark font-weight-bolder ms-sm-3' : 'text-white font-weight-bolder ms-sm-3']"
         to="/"
         rel="tooltip"
-        title="Navbar Pc"
+        title="Navbar"
         data-placement="bottom"
       >
         ELEVATE
       </RouterLink>
-      <RouterLink
-        class="navbar-brand d-block d-md-none"
-        :class="
-          props.transparent || props.dark
-            ? 'text-white'
-            : 'font-weight-bolder ms-sm-3'
-        "
-        to="/"
-        rel="tooltip"
-        title="Navbar cel"
-        data-placement="bottom"
+
+      <!-- Botón de hamburguesa en pantallas pequeñas -->
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navigation"
+        aria-controls="navigation"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
       >
-        ELEVATE
-      </RouterLink>
-      <div
-        class="collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0"
-        id="navigation"
-      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0" id="navigation">
         <ul class="navbar-nav navbar-nav-hover ms-auto">
+          <!-- Cursos Button -->
           <li class="nav-item mx-2">
             <router-link
               role="button"
@@ -173,14 +159,12 @@ const showProfileButton = computed(() => isLoggedIn.value);
               :class="getTextColor()"
               to="/courses"
             >
-              <i
-                class="material-icons opacity-6 me-2 text-md"
-                :class="getTextColor()"
-                >book</i
-              >
+              <i class="material-icons opacity-6 me-2 text-md" :class="getTextColor()">book</i>
               Cursos
             </router-link>
           </li>
+
+          <!-- Pages Dropdown -->
           <li class="nav-item dropdown dropdown-hover mx-2">
             <a
               role="button"
@@ -190,77 +174,30 @@ const showProfileButton = computed(() => isLoggedIn.value);
               data-bs-toggle="dropdown"
               aria-expanded="false"
             >
-              <i
-                class="material-icons opacity-6 me-2 text-md"
-                :class="getTextColor()"
-                >dashboard</i
-              >
-              Paginas
-              <img
-                :src="getArrowColor()"
-                alt="down-arrow"
-                class="arrow ms-2 d-lg-block d-none"
-              />
-              <img
-                :src="getArrowColor()"
-                alt="down-arrow"
-                class="arrow ms-1 d-lg-none d-block ms-auto"
-              />
+              <i class="material-icons opacity-6 me-2 text-md" :class="getTextColor()">dashboard</i>
+              Páginas
+              <img :src="getArrowColor()" alt="down-arrow" class="arrow ms-2" />
             </a>
-            <div
-              class="dropdown-menu dropdown-menu-animation ms-n3 dropdown-md p-3 border-radius-xl mt-0 mt-lg-3"
-              aria-labelledby="dropdownMenuPages"
-            >
-              <div class="row d-none d-lg-block">
-                <div class="col-12 px-4 py-2">
-                  <div class="row">
-                    <div class="position-relative">
-                      <div
-                        class="dropdown-header text-dark font-weight-bolder d-flex align-items-center px-1"
-                      >
-                        Paginas de destino
-                      </div>
-                      <RouterLink
-                        :to="{ name: 'about' }"
-                        class="dropdown-item border-radius-md"
-                      >
-                        <span>Sobre nosotros</span>
-                      </RouterLink>
-                      <RouterLink
-                        :to="{ name: 'contactus' }"
-                        class="dropdown-item border-radius-md"
-                      >
-                        <span>Contactanos</span>
-                      </RouterLink>
-
-                      <div v-if="isLoggedIn"></div>
-
-                      <div class="RegisterLogin" v-else>
-                        <div
-                          class="dropdown-header text-dark font-weight-bolder d-flex align-items-center px-0 mt-3"
-                        >
-                          Cuenta
-                        </div>
-                        <RouterLink
-                          :to="{ name: 'login-view' }"
-                          class="dropdown-item border-radius-md"
-                        >
-                          <span>Iniciar Sesion</span>
-                        </RouterLink>
-                        <RouterLink
-                          :to="{ name: 'register-view' }"
-                          class="dropdown-item border-radius-md"
-                        >
-                          <span>Registro</span>
-                        </RouterLink>
-                      </div>
-                    </div>
-                  </div>
+            <div class="dropdown-menu dropdown-menu-animation ms-n3 dropdown-md p-3 border-radius-xl mt-0 mt-lg-3" aria-labelledby="dropdownMenuPages">
+              <!-- Contenido de dropdown -->
+              <RouterLink :to="{ name: 'about' }" class="dropdown-item">Sobre nosotros</RouterLink>
+              <RouterLink :to="{ name: 'contactus' }" class="dropdown-item">Contactanos</RouterLink>
+              <!-- Mostrar "Iniciar Sesión" y "Registro" cuando no esté logueado -->
+              <div v-if="!isLoggedIn">
+                <div class="dropdown-header text-dark font-weight-bolder d-flex align-items-center px-0 mt-3">
+                  Cuenta
                 </div>
+                <RouterLink :to="{ name: 'login-view' }" class="dropdown-item border-radius-md">
+                  <span>Iniciar Sesión</span>
+                </RouterLink>
+                <RouterLink :to="{ name: 'register-view' }" class="dropdown-item border-radius-md">
+                  <span>Registro</span>
+                </RouterLink>
               </div>
-              <!-- fin de paginas de pc -->              
             </div>
           </li>
+
+          <!-- Dashboard Button for Logged In Users -->
           <li v-if="isLoggedIn" class="nav-item mx-2">
             <router-link
               role="button"
@@ -268,16 +205,14 @@ const showProfileButton = computed(() => isLoggedIn.value);
               :class="getTextColor()"
               to="/dashboard"
             >
-              <i
-                class="material-icons opacity-6 me-2 text-md"
-                :class="getTextColor()"
-                >person</i
-              >
+              <i class="material-icons opacity-6 me-2 text-md" :class="getTextColor()">person</i>
               Panel
             </router-link>
           </li>
+
+          <!-- Logout Button -->
           <li v-if="isLoggedIn">
-            <button @click="cerrarSesion" class="btn btn-danger ms-2" href="/">
+            <button @click="cerrarSesion" class="btn btn-danger ms-2">
               Cerrar Sesión
             </button>
           </li>
@@ -286,3 +221,8 @@ const showProfileButton = computed(() => isLoggedIn.value);
     </div>
   </nav>
 </template>
+
+
+
+
+
