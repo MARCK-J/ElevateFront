f9c0dcf061a0774888e639de6fa6cbab7b74e769
FerFisX <aacarapir2@fcpn.edu.bo>
2024-12-11 10:57:23 -0400

    Correccion Bug seccion About US que mandaba a otra pagina que no era de la empresa

--- src/examples/footers/FooterCentered.vue
+++ src/examples/footers/FooterCentered.vue
@@ -1,46 +1,18 @@
 <script setup>
 defineProps({
-  socials: {
-    type: Array,
-    icon: String,
-    link: String,
-    default: () => [
-      {
-        icon: '<i class="fab fa-facebook text-lg opacity-8"></i>',
-        link: "https://www.facebook.com/CreativeTim/",
-      },
-      {
-        icon: '<i class="fab fa-twitter text-lg opacity-8"></i>',
-        link: "https://twitter.com/creativetim",
-      },
-      {
-        icon: '<i class="fab fa-dribbble text-lg opacity-8"></i>',
-        link: "https://dribbble.com/creativetim",
-      },
-      {
-        icon: '<i class="fab fa-github text-lg opacity-8"></i>',
-        link: "https://github.com/creativetimofficial",
-      },
-      {
-        icon: '<i class="fab fa-youtube text-lg opacity-8"></i>',
-        link: "https://www.youtube.com/channel/UCVyTG4sCw-rOvB9oHkzZD1w",
-      },
-    ],
-  },
+  
   links: {
     type: Array,
     name: String,
-    href: String,
+    nombreRutas: String,
     default: () => [
-      { href: "https://www.creative-tim.com/", name: "Company" },
-      { href: "https://www.creative-tim.com/presentation", name: "About Us" },
-      { href: "https://www.creative-tim.com/presentation", name: "Team" },
+      { nombreRutas: "/", name: "Home" },
+      { nombreRutas: "/pages/about-us", name: "About Us" },
       {
-        href: "https://www.creative-tim.com/templates/vuejs",
-        name: "Products",
+        nombreRutas: "/pages/register",
+        name: "Registrate",
       },
-      { href: "https://www.creative-tim.com/blog", name: "Blog" },
-      { href: "https://www.creative-tim.com/license", name: "License" },
+      { nombreRutas: "/pages/contact-us", name: "Contactanos" },
     ],
   },
 });
@@ -50,32 +22,20 @@ defineProps({
     <div class="container">
       <div class="row">
         <div class="col-lg-8 mb-4 mx-auto text-center">
-          <a
-            v-for="{ name, href } of links"
+          <router-link 
+            v-for="{ name, nombreRutas } of links"
             :key="name"
-            :href="href"
-            target="_blank"
+            :to=nombreRutas
             class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2"
           >
             {{ name }}
-          </a>
-        </div>
-        <div class="col-lg-8 mx-auto text-center mb-4 mt-2">
-          <a
-            v-for="{ icon, link } of socials"
-            :key="link"
-            :href="link"
-            target="_blank"
-            class="text-secondary me-xl-4 me-4"
-            v-html="icon"
-          >
-          </a>
+          </router-link>
         </div>
       </div>
       <div class="row">
         <div class="col-8 mx-auto text-center mt-1">
           <p class="mb-0 text-secondary">
-            Copyright © {{ new Date().getFullYear() }} Material by Creative Tim.
+            Copyright © {{ new Date().getFullYear() }}Elevate Company
           </p>
         </div>
       </div>
